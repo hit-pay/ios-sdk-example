9d4b13fabb49e1d2383a1d096663ef6a7c0a1b28
Tuan Nguyen <tuannguyenanh177@gmail.com>
2021-07-24 14:50:16 +0700

    Initial commit

---
+++ Example/HitPay-iOS-SDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HitPay-iOS-SDK
+//
+//  Created by 1bannamgiauten on 07/24/2021.
+//  Copyright (c) 2021 1bannamgiauten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
